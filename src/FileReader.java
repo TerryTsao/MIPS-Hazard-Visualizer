d8befd9ebf72da18e7ec461c94c621f872c8d259
orrblue <nitzan.orr@gmail.com>
2016-05-24 11:34:01 -0700

    Added elementary file reader

--- src/FileReader.java
+++ src/FileReader.java
@@ -6,6 +6,7 @@ import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.Component;
 import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.*;
@@ -16,43 +17,56 @@ public class FileReader
 {
    static File asmFile;
    static Path asmFilePath;
-   
-   
+
+
    public FileReader(){
-      
+
    }
-   
+
    public static void openFileChooser() {
       JFileChooser chooser = new JFileChooser();
       //UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
       int returnVal = chooser.showOpenDialog(null);
       if(returnVal == JFileChooser.APPROVE_OPTION) {
          System.out.println("You chose to open this file: " +
-              chooser.getSelectedFile().getName());
+               chooser.getSelectedFile().getName());
          asmFile = chooser.getSelectedFile();
          asmFilePath = Paths.get( asmFile.getPath());
          System.out.println(asmFilePath);
+         readFile();
       }
    }
-   
+
+   private static void readFile() {
+      Path file = asmFilePath;
+      try (InputStream in = Files.newInputStream(file);
+            BufferedReader reader =
+                  new BufferedReader(new InputStreamReader(in))) {
+         String line = null;
+         while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+         }
+      } catch (IOException x) {
+         System.err.println(x);
+      }
+   }
+
    public static File getFile() {
       return asmFile;
    }
-   
+
    public static Path getPath() {
       return asmFilePath;
    }
-   
-      
-      
-   
-   public static void main(String [] args)
-   {
-      
+
+   /**
+    * 
+    */
+   public static void setLookAndFeel() {
       try {
          // Set cross-platform Java L&F (also called "Metal")
          UIManager.setLookAndFeel(
-               UIManager.getCrossPlatformLookAndFeelClassName());
+               "javax.swing.plaf.nimbus.NimbusLookAndFeel");
       } 
       catch (UnsupportedLookAndFeelException e) {
          // handle exception
@@ -66,18 +80,26 @@ public class FileReader
       catch (IllegalAccessException e) {
          // handle exception
       }
-      
-       FileReader.openFileChooser();
-      
-      
-      
-      
-      
    }
 
-   
-   
-   
-   
-   
+
+
+
+   public static void main(String [] args)
+   {
+
+      FileReader.setLookAndFeel();
+      FileReader.openFileChooser();
+
+
+
+
+
+   }
+
+
+
+
+
+
 }
