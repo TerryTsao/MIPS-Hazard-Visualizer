5a3d97a047e119cef455cd43f9df3b32e685c054
TerryTsao <xxc142@case.edu>
2016-06-06 10:10:27 -0700

    Simple prototype of two panels
    prototype of two panels together
    
    simple tester used for instrPanel

--- src/gui/InstructionPanel.java
+++ src/gui/InstructionPanel.java
@@ -9,16 +9,19 @@ import java.awt.RenderingHints;
 
 import javax.swing.JPanel;
 
-import data.Instruction;
 import data.InstructionList;
 
 public class InstructionPanel extends JPanel {
    public static final int NUM_OF_LINES = 5;
 
+   public static final int INDENT = 20;
+
    private String[] instructions;
    private InstructionList iListReference;
 
    public InstructionPanel() {
+      setPreferredSize(
+            new Dimension(GUIConstants.PIPELINE_PANEL_REF_WIDTH / 3, 900));
       instructions = new String[NUM_OF_LINES];
       for (int i = 0; i < NUM_OF_LINES; i++) {
          instructions[i] = new String("add $t0 $t1 $t2");
@@ -40,21 +43,23 @@ public class InstructionPanel extends JPanel {
       setOpaque(true);
       setPreferredSize(new Dimension(300,900));
 
-      double xScale = getWidth() 
-            / (double)GUIConstants.INSTRUCTION_PANEL_REF_WIDTH;
-      double yScale = getHeight() 
-            / (double)GUIConstants.INSTRUCTION_PANEL_REF_HEIGHT;
-      g2d.scale(xScale, yScale);
+      // double xScale = getWidth()
+      // / (double)GUIConstants.INSTRUCTION_PANEL_REF_WIDTH;
+      // double yScale = getHeight()
+      // / (double)GUIConstants.INSTRUCTION_PANEL_REF_HEIGHT;
+      // g2d.scale(xScale, yScale);
 
       // Anti-aliasing
       g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
             RenderingHints.VALUE_ANTIALIAS_ON);
       g2d.setColor(Color.black);
-      g2d.setFont(new Font("Arial", Font.BOLD, 24));
+      g2d.setFont(new Font("Arial", Font.BOLD, 30));
 
       for (int i = 0; i < NUM_OF_LINES; i++) {
-         g2d.drawString(instructions[i], 10, 100 + 80 * i);
-         System.out.println(instructions[i]);
+         int y = (int)(ProcessorDiagram.ORIGIN.y + ProcessorDiagram.Y_DISTANCE
+                     * (i + 0.7) * ProcessorDiagram.SCALE_RATIO);
+         g2d.drawString(instructions[i], INDENT, y);
+         System.out.println(y);
       }
    }
 }
---
+++ src/gui/tester/TestInstructPanel.java
@@ -0,0 +1,28 @@
+package gui.tester;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import gui.GUIConstants;
+import gui.InstructionPanel;
+
+public class TestInstructPanel {
+   public static void main(String[] args) {
+      // FileReader.setLookAndFeel();
+      // FileReader.openFileChooser();
+      SwingUtilities.invokeLater(new Runnable() {
+         @Override
+         public void run() {
+            JFrame frame = new JFrame("MIPS Hazard Visualizer");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(GUIConstants.INIT_FRAME_WIDTH,
+                  GUIConstants.INIT_FRAME_HEIGHT);
+            frame.setVisible(true);
+            InstructionPanel ip = new InstructionPanel();
+            frame.add(ip, BorderLayout.WEST);
+         }
+      });
+   }
+}
