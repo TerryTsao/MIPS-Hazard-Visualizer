e0f0cc338d6fc6599b090b81de8e4234b011a46f
TerryTsao <xxc142@case.edu>
2016-06-16 01:12:43 -0700

    prototype of split print instructions

--- src/gui/InstructionPanel.java
+++ src/gui/InstructionPanel.java
@@ -9,6 +9,7 @@ import java.awt.RenderingHints;
 
 import javax.swing.JPanel;
 
+import data.Instruction;
 import data.InstructionList;
 
 public class InstructionPanel extends JPanel {
@@ -16,20 +17,20 @@ public class InstructionPanel extends JPanel {
 
    public static final int INDENT = 30;
 
-   private String[] instructions;
+   private Instruction[] instructions;
 
    public InstructionPanel() {
       setPreferredSize(
             new Dimension(GUIGlobal.PIPELINE_PANEL_REF_WIDTH / 3, 900));
 
-      instructions = new String[NUM_OF_LINES];
+      instructions = new Instruction[NUM_OF_LINES];
       fetchInstructions();
    }
 
    public void fetchInstructions() {
       for (int i = 0; i < NUM_OF_LINES; i++) {
-         instructions[i] = InstructionList.getInstructionAtIndex(
-               InstructionList.getProgramCounter() + i);
+         instructions[i] = InstructionList.getList()
+               .get(InstructionList.getProgramCounter() + i);
       }
    }
 
@@ -52,10 +53,39 @@ public class InstructionPanel extends JPanel {
       g2d.setColor(Color.black);
       g2d.setFont(new Font("Arial", Font.BOLD, 30));
 
+      // for (int i = 0; i < NUM_OF_LINES; i++) {
+      // int y = (int)(ProcessorDiagram.ORIGIN.y + ProcessorDiagram.Y_DISTANCE
+      // * (i + 0.69) * ProcessorDiagram.SCALE_RATIO);
+      // g2d.drawString(instructions[i], INDENT, y);
+      // }
+      printInstructions(g2d);
+   }
+
+   private void printInstructions(Graphics2D g2d) {
       for (int i = 0; i < NUM_OF_LINES; i++) {
-         int y = (int)(ProcessorDiagram.ORIGIN.y + ProcessorDiagram.Y_DISTANCE
-                     * (i + 0.69) * ProcessorDiagram.SCALE_RATIO);
-         g2d.drawString(instructions[i], INDENT, y);
+         String[] parts = {
+               instructions[i].getCmd(),
+               instructions[i].getArg1(),
+               instructions[i].getArg2(),
+               instructions[i].getArg3()
+               };
+         drawParts(parts, i, g2d);
+      }
+   }
+
+   private void drawParts(String[] parts, int level, Graphics2D g2d) {
+      int y = (int)(ProcessorDiagram.ORIGIN.y + ProcessorDiagram.Y_DISTANCE
+            * (level + 0.69) * ProcessorDiagram.SCALE_RATIO);
+      int x = 0;
+      for (int i = 0; i < parts.length; i++) {
+         if (parts[i] == null)
+            continue;
+         // TODO: add validator
+         if (true)
+            g2d.setColor(Color.black);
+         else
+            g2d.setColor(Color.red);
+         g2d.drawString(parts[i], x += INDENT * 2, y);
       }
    }
 }
